3e53b2491f1016cb9f55992b4bb3e00d982e38f4
Dmytro Symonenko <Dmytro_Symonenko@epam.com>
2017-11-10 12:59:08 +0200

    xex

--- provision.sh
+++ provision.sh
@@ -3,6 +3,8 @@
 AWS_IP=$(cat ../aws_ip.conf)
 AWS_ID=$(cat ../aws_id.conf)
 KILL_PROVISION=$(cat ../param.conf | grep "KILL" | awk '{print $2}')
+TAG=$(cat ../param.conf | grep "TAG" | awk '{print $2}')
+KEY=$(cat ../param.conf | grep "KEY" | awk '{print $2}')
 
 echo $PWD ${PWD}
 echo $TAG ${TAG}
@@ -10,7 +12,7 @@ echo $KEY ${KEY}
 
 cd lamp_simple
 printf "[all]\n$AWS_IP" > hosts
-ansible-playbook -i ${AWS_IP} site.yml
+ansible-playbook -i hosts site.yml
 
 if [[ ${KILL_PROVISION} == "True" ]]; then
   echo "killing box "
