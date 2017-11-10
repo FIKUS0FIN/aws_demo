81d10159cfa098ba3397accbb34495ed546b4f8f
Dmytro Symonenko <Dmytro_Symonenko@epam.com>
2017-11-10 12:48:04 +0200

    dad

--- provision.sh
+++ provision.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
 AWS_IP=$(cat ../aws_ip.conf)
-
 AWS_ID=$(cat ../aws_id.conf)
+KILL_PROVISION=$(cat ../param.conf | grep "KILL" | awk '{print $2}')
 
 echo $PWD ${PWD}
 echo $TAG ${TAG}
 echo $KEY ${KEY}
 
-KILL_PROVISION=$(cat ../param.conf | grep "KILL" | awk '{print $2}')
-
-#PARAM=$(echo ${JOB_NAME} | cut -d " " -f2)
-
+cd lamp_simple
+printf "[all]\n$AWS_IP" > hosts
 ansible-playbook -i ${AWS_IP} site.yml
 
 if [[ ${KILL_PROVISION} == "True" ]]; then
