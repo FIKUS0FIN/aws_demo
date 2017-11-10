0e96141a7d33d7782aef522ee3c5cb0787c52d78
Dmytro Symonenko <Dmytro_Symonenko@epam.com>
2017-11-10 13:56:38 +0200

    new staff

--- provision.sh
+++ provision.sh
@@ -11,10 +11,13 @@ echo $TAG ${TAG}
 echo $KEY ${KEY}
 echo $AWS_DNS
 
-cd 00-simple-playbook-examples
+cd ansible
+echo $PWD
 #printf "$AWS_IP" >> hosts
-ansible-playbook -i hosts prepare_ansible_target.yml
-ansible-playbook prepare_ansible_target.yml -i hosts --extra-vars "all=$AWS_IP"
+#ansible-playbook -i hosts prepare_ansible_target.yml
+#ansible-playbook prepare_ansible_target.yml -i hosts --extra-vars "all=$AWS_DNS"
+ansible-playbook -i $AWS_DNS, jenkins.yml
+
 if [[ ${KILL_PROVISION} == "True" ]]; then
   echo "killing box "
   aws ec2 terminate-instances --instance-ids $AWS_ID
