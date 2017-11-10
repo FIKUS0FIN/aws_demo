7393e66f7cb64ce05f8aa3faa924b63b39bb81e1
Dmytro Symonenko <Dmytro_Symonenko@epam.com>
2017-11-10 13:20:04 +0200

    ded

--- provision.sh
+++ provision.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-AWS_IP=$(cat ../aws_ip.conf)
+AWS_DNS=$(cat ../aws_DNS.conf)
 AWS_ID=$(cat ../aws_id.conf)
 KILL_PROVISION=$(cat ../param.conf | grep "KILL" | awk '{print $2}')
 TAG=$(cat ../param.conf | grep "TAG" | awk '{print $2}')
@@ -9,10 +9,11 @@ KEY=$(cat ../param.conf | grep "KEY" | awk '{print $2}')
 echo $PWD ${PWD}
 echo $TAG ${TAG}
 echo $KEY ${KEY}
+echo $AWS_DNS
 
-cd lamp_simple
-printf "[all]\n$AWS_IP" > hosts
-ansible-playbook -i hosts site.yml
+#cd lamp_simple
+#printf "$AWS_IP" >> hosts
+#ansible-playbook -i host site.yml
 
 if [[ ${KILL_PROVISION} == "True" ]]; then
   echo "killing box "
