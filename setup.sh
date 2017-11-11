ba467a00340de9d2fe62240d53f0d155f725d35d
Dmytro Symonenko <Dmytro_Symonenko@epam.com>
2017-11-11 15:06:01 +0200

    done

--- setup.sh
+++ setup.sh
@@ -1,34 +1,57 @@
 #!/bin/bash
 
-## creating Korobka
-AWS_ID=$(aws ec2 run-instances   --image-id ami-1e339e71 --key-name jenkins --security-groups EC2SecurityGroup --instance-type t2.micro --output text | grep INSTANCES | awk '{print $7}' )
-echo "KOROBKA ID is :: $AWS_ID"
+##functions
+#-----------------------------------------------------------------------------
 
-## checking Koroaka state
-while [[ $(aws ec2 describe-instances --query 'Reservations[*].Instances[*].[State.Name, InstanceId]' --output text | grep "running" | grep "$AWS_ID" | wc -l) -eq 0 ]]
-do
-  sleep 2
-  echo "WATING FOR KOROBKA"
-done
-echo "Kobobka is running"
-sleep 15
+function grep_aws_id
+{
+  AWS_ID=$(aws ec2 run-instances   --image-id ami-1e339e71 --key-name jenkins --security-groups EC2SecurityGroup --instance-type t2.micro --output text | grep INSTANCES | awk '{print $7}' )
+  echo "KOROBKA ID is :: $AWS_ID"
+}
 
-## Adding TAG to Korobka
-echo "Taging Korobka with Key and Tag  :: ${TAG} :: ${KEY} "
-aws ec2 create-tags --resources $AWS_ID --tags Key=${KEY},Value=${TAG}
+function checking_aws_state
+{
+  while [[ $(aws ec2 describe-instances --query 'Reservations[*].Instances[*].[State.Name, InstanceId]' --output text | grep "running" | grep "$1" | wc -l) -eq 0 ]]
+  do
+    sleep 2
+    echo "WATING FOR KOROBKA"
+  done
+  echo "Kobobka is running"
+  sleep 15
+}
 
-## getting IP-addr Instance
-AWS_DNS=$(aws ec2 describe-instances --output text --instance-ids $AWS_ID | grep ASSOCIATION | awk '{print $3}' | sort -u)
-echo "getting DNS of the Instance :: $AWS_DNS "
+function taging_aws_box
+{
+  echo "Taging Korobka with Key and Tag  :: ${TAG} :: ${KEY} "
+  aws ec2 create-tags --resources $1 --tags Key=${KEY},Value=${TAG}
+}
 
-## getting result form Korobka
-aws ec2 describe-instances --instance-ids $AWS_ID
+function getting_dns_aws_box
+{
+  AWS_DNS=$(aws ec2 describe-instances --output text --instance-ids $1 | grep ASSOCIATION | awk '{print $3}' | sort -u)
+  echo "getting DNS of the Instance :: $AWS_DNS "
+}
 
-## terminating Korobka
-if [[ ${KILL} == "True" ]]; then
-  echo "killing box "
-  aws ec2 terminate-instances --instance-ids $AWS_ID
-fi
+function aws_result
+{
+  aws ec2 describe-instances --instance-ids $AWS_ID
+}
+
+function kill_aws ()
+{
+  if [[ $1 == "True" ]]; then
+    echo "killing box "
+    aws ec2 terminate-instances --instance-ids $AWS_ID
+  fi
+}
+
+##ARGS
+#-----------------------------------------------------------------------------
+
+grep_aws_id $AWS_ID
+taging_aws_box $AWS_ID
+getting_dns_aws_box $AWS_ID
+kill_aws $KILL
 
 echo $AWS_ID > ../aws_id.conf
 echo $AWS_DNS > ../aws_DNS.conf
--- test.sh
+++ test.sh
@@ -1,10 +1,73 @@
 #!/bin/bash
 echo "start"
+echo "function name kill_aws"
 
 aws=1
 
-function KILL ($aws) {
+function kill_aws ()
+{
   if [[ $1 == "1" ]]; then
     echo "killing box "
+    echo "$1"
   fi
 }
+
+kill_aws $aws
+
+function aws_test
+{
+  echo "start aws_test"
+  aws=212
+  #statements
+}
+
+aws_test
+echo $aws
+
+function grep_aws_id
+{
+  #-----------------------------------------------------------------------------
+  AWS_ID=$(aws ec2 run-instances   --image-id ami-1e339e71 --key-name jenkins --security-groups EC2SecurityGroup --instance-type t2.micro --output text | grep INSTANCES | awk '{print $7}' )
+  echo "KOROBKA ID is :: $AWS_ID"
+}
+
+function checking_aws_state
+{
+  while [[ $(aws ec2 describe-instances --query 'Reservations[*].Instances[*].[State.Name, InstanceId]' --output text | grep "running" | grep "$1" | wc -l) -eq 0 ]]
+  do
+    sleep 2
+    echo "WATING FOR KOROBKA"
+  done
+  echo "Kobobka is running"
+  sleep 15
+}
+
+function taging_aws_box
+{
+  echo "Taging Korobka with Key and Tag  :: ${TAG} :: ${KEY} "
+  aws ec2 create-tags --resources $1 --tags Key=${KEY},Value=${TAG}
+}
+
+function getting_dns_aws_box
+{
+  AWS_DNS=$(aws ec2 describe-instances --output text --instance-ids $1 | grep ASSOCIATION | awk '{print $3}' | sort -u)
+  echo "getting DNS of the Instance :: $AWS_DNS "
+}
+
+function aws_result
+{
+  aws ec2 describe-instances --instance-ids $AWS_ID
+}
+
+function kill_aws ()
+{
+  if [[ $1 == "True" ]]; then
+    echo "killing box "
+    aws ec2 terminate-instances --instance-ids $AWS_ID
+  fi
+}
+
+grep_aws_id $AWS_ID
+taging_aws_box $AWS_ID
+getting_dns_aws_box $AWS_ID
+kill_aws $KILL
