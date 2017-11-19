491efa235b5d9b561cce75c84fad0f002b941b1c
Dmytro Symonenko <Dmytro_Symonenko@epam.com>
2017-11-19 17:17:21 +0200

    aw_less.sh corrections

--- aw_less.sh
+++ aw_less.sh
@@ -5,20 +5,24 @@
 #securitygroup=EC2SecurityGroup \
 
 
-function define_profision_file
+function maine
 {
-  if [[ $TAG=="none"]]; then
-    yes | awless create instance name=$NAME image=ami-3f1bd150 keypair=jenkins_2 \
-    subnet=subnet-4e64b325 securitygroup=@EC2SecurityGroup \
+  if [[ $TAG == "none" ]]; then
+    yes | awless create instance name=${NAME}*:_:*${TAG} image=ami-3f1bd150 keypair=jenkins_2 \
+    subnet=subnet-4e64b325 securitygroup=@EC2SecurityGroup > raw.txt
     #statements
   else
-    yes | awless create instance name=$NAME image=ami-3f1bd150 keypair=jenkins_2 \
+    yes | awless create instance name=${NAME}*:_:*${TAG} image=ami-3f1bd150 keypair=jenkins_2 \
     subnet=subnet-4e64b325 securitygroup=@EC2SecurityGroup \
-    userdata=https://raw.githubusercontent.com/FIKUS0FIN/aws_demo/master/provisionign_pakeges/${TAG}.sh
+    userdata=https://raw.githubusercontent.com/FIKUS0FIN/aws_demo/master/provisionign_pakeges/${TAG}.sh > raw.txt
   fi
+
+  AWS_ID=$(cat raw.txt | grep "instance = " | awk '{print $6}')
+  yes | awless check instance id=$AWS_ID state=running
+  sleep 10 
 }
 
-define_profision_file
+maine
 
 #yes | awless create instance name=nginx image=ami-3f1bd150 keypair=jenkins_2 \
 #subnet=subnet-4e64b325 securitygroup=@EC2SecurityGroup \
