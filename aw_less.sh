f1b76021e5800cb06b75076403a574ea3fdd3139
Dmytro Symonenko <Dmytro_Symonenko@epam.com>
2017-11-19 11:37:53 +0200

    aw_less corrections

--- aw_less.sh
+++ aw_less.sh
@@ -4,6 +4,6 @@
 #awless create instance name=Jenkins image=ami-1e339e71 keypair=jenkins_2 \
 #securitygroup=EC2SecurityGroup \
 
-awless create instance name=my_machine image=ami-3f1bd150 keypair={keypair.name} \
-subnet={main.subnet} securitygroup={securitygroup} \
-userdata=https://gist.github.com/FIKUS0FIN/985f88fb0c99dae4374976d0c52a06de
+awless create instance name=nginx image=ami-3f1bd150 keypair=jenkins_2 \
+subnet=subnet-4e64b325 securitygroup=@EC2SecurityGroup \
+userdata=https://raw.githubusercontent.com/FIKUS0FIN/aws_demo/master/py_min.sh
