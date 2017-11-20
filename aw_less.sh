efbc32b3fc03325857ff46d404163e1c5a62f6ae
Dmytro Symonenko <Dmytro_Symonenko@epam.com>
2017-11-20 04:43:17 +0200

    add comments to script aw_less

--- aw_less.sh
+++ aw_less.sh
@@ -4,10 +4,15 @@
 #awless create instance name=Jenkins image=ami-1e339e71 keypair=jenkins_2 \
 #securitygroup=EC2SecurityGroup \
 
-
+--------------------------------------------------------------------------------
 function maine
 {
-  if [[ $TAG == "none" ]]; then
+  # create Amazon EC2 instance, Name it and *Tag in to do
+  # grep AWS_ID and primary provisioning * none.sh simple-python pakege *
+  # wait antil state == running
+  # KILL function * terminate-instances if ${KILL} if == "TRUE"
+  ##----------------------------------------------------------------------------
+  if [[ ${TAG} == "none" ]]; then
     yes | awless create instance name=${NAME}*:_:*${TAG} image=ami-3f1bd150 keypair=jenkins_2 \
     subnet=subnet-4e64b325 securitygroup=@EC2SecurityGroup > raw.txt
     #statements
@@ -19,10 +24,19 @@ function maine
 
   AWS_ID=$(cat raw.txt | grep "instance = " | awk '{print $6}')
   yes | awless check instance id=$AWS_ID state=running
-  sleep 10 
+  sleep 10
 }
-
+function kill_aws ()
+{
+  if [[ ${KILL} == "True" ]]; then
+    echo "killing box "
+    aws ec2 terminate-instances --instance-ids $AWS_ID
+  fi
+  #statements
+}
+--------------------------------------------------------------------------------
 maine
+kill_aws ${KILL}
 
 #yes | awless create instance name=nginx image=ami-3f1bd150 keypair=jenkins_2 \
 #subnet=subnet-4e64b325 securitygroup=@EC2SecurityGroup \
