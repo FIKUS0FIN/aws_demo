d3a1b2ab07ecd697d462a7fae6b0d15f6a665dd5
Dmytro Symonenko <Dmytro_Symonenko@epam.com>
2018-01-03 12:42:19 +0200

    new nginx_script

--- demo_ansible/new_ansible_folder/files/nginx/proxied_tomcat.sh
+++ demo_ansible/new_ansible_folder/files/nginx/proxied_tomcat.sh
@@ -13,21 +13,8 @@ sudo add-apt-repository -y ppa:nginx/stable
 sudo apt-get update
 sudo apt-get -y install nginx
 cp local.conf /etc/nginx/conf.d/local.conf
-mkdir -p /etc/nginx/ssl
-cp ssl.rules /etc/nginx/ssl/ssl.rules
 cp nginx.conf /etc/nginx/nginx.conf
 
-# Generate the Keys
-
-    mkdir -p /etc/nginx/ssl/keys
-    openssl genpkey -algorithm RSA -out /etc/nginx/ssl/keys/private.key -pkeyopt rsa_keygen_bits:2048
-    openssl rsa -in /etc/nginx/ssl/keys/private.key -out /etc/nginx/ssl/keys/private-decrypted.key
-    openssl req -new -sha256 -key /etc/nginx/ssl/keys/private-decrypted.key -subj "/CN=$SERVER_NAME" -out /etc/nginx/ssl/keys/$SERVER_NAME.csr
-    openssl x509 -req -days 365 -in /etc/nginx/ssl/keys/$SERVER_NAME.csr -signkey /etc/nginx/ssl/keys/private.key -out /etc/nginx/ssl/keys/server.crt
-    rm /etc/nginx/ssl/keys/private-decrypted.key
-    rm /etc/nginx/ssl/keys/$SERVER_NAME.csr
-
-openssl dhparam -outform pem -out /etc/nginx/ssl/dhparam2048.pem 2048
 
 sed -i "s/SERVER_NAME/$SERVER_NAME/" /etc/nginx/conf.d/local.conf
 sed -i "s/PORT_NUMBER/$OPT_A/" /etc/nginx/conf.d/local.conf
