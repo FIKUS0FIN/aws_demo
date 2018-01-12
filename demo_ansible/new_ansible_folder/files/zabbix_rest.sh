3d732ae195276c197431a1ac50e141faf74f89ef
Dmytro Symonenko <Dmytro_Symonenko@epam.com>
2018-01-12 17:06:07 +0200

    done staff

--- demo_ansible/new_ansible_folder/files/zabbix_rest.sh
+++ demo_ansible/new_ansible_folder/files/zabbix_rest.sh
@@ -23,15 +23,14 @@ create_host() {
   curl -i -X POST -H 'Content-Type: application/json-rpc' -d "{\"jsonrpc\":\"2.0\",\"method\":\"host.create\",\"params\":{\"host\":\"$HOST_NAME\",\"interfaces\": [{\"type\": 1,\"main\": 1,\"useip\": 1,\"ip\": \"$IP_ADDRESS\",\"dns\": \"\",\"port\": \"10050\"}],\"groups\": [{\"groupid\": \"10\"}]},\"auth\":\"$AUTH_TOKEN\",\"id\":0}" $API
 }
 
+
 AUTH_TOKEN=`echo $(authenticate)|jq -r .result`
 
 GET_HOST=$(get_host)
 
 CREAT=$(create_host)
 
+
 echo "$AUTH_TOKEN"
 echo "$GET_HOST"
 echo "$CREAT" > zabbix.conf
-
-
-#echo "$GET_HOST"   # DEBUG PART
--- provisionign_pakeges/primary_setup.sh
+++ provisionign_pakeges/primary_setup.sh
@@ -2,7 +2,7 @@
 sudo apt update
 sudo apt-get install -y --no-install-recommends python-minimal
 sudo apt-get install ansible -y
-sudo apt-get install jq -y 
+sudo apt-get install jq -y
 
 ################################################################################
 #add zabixx monitor _ auto registration
@@ -12,7 +12,26 @@ date >> ~/add_monitor.log
 echo "add_monitor wass secsessfull" >> ~/add_monitor.log" > /usr/local/bin/add_monitor.sh
 
 echo "#!/bin/bash
-date >> ~/dell_monitor.log
+HOST_ID=$(cat /home/ubuntu/zabbix.conf | grep hostids |  cut -d"\"" -f10
+USER='Admin'
+PASS='admin'
+ZABBIX_SERVER='35.205.201.49'
+API='http://35.205.201.49/zabbix/api_jsonrpc.php'
+
+# Authenticate with Zabbix API
+
+authenticate() {
+  echo `curl -s -H  'Content-Type: application/json-rpc' -d "{\"jsonrpc\": \"2.0\",\"method\":\"user.login\",\"params\":{\"user\":\""${USER}"\",\"password\":\""${PASS}"\"},\"auth\": null,\"id\":0}" $API`
+}
+
+dell_host(){
+  curl -i POST -H 'Content-Type: application/json-rpc' -d "{\"jsonrpc\": \"2.0\",\"method\": \"host.delete\",\"params\": [\"$HOST_ID\"],\"auth\": \"$AUTH_TOKEN\",\"id\": 1}" $API
+}
+
+AUTH_TOKEN=`echo $(authenticate)|jq -r .result`
+
+DELL_HOST=$(dell_host)
+
 echo "dell_monitor wass secsessfull" >> ~/dell_monitor.log" > /usr/local/bin/dell_monitor.sh
 
 #### adding files config
