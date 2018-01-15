7b61d3aa4212e4c13317207b432f51e85758348a
Dmytro Symonenko <Dmytro_Symonenko@epam.com>
2018-01-15 11:18:25 +0200

    end

--- provisionign_pakeges/primary_setup.sh
+++ provisionign_pakeges/primary_setup.sh
@@ -2,26 +2,4 @@
 sudo apt update
 sudo apt-get install -y --no-install-recommends python-minimal
 sudo apt-get install ansible -y
-apt install jq -y 
-
-################################################################################
-#add zabixx monitor _ auto registration
-
-echo "[Unit]
-Before=shutdown.target reboot.target
-After=network.target
-Requires=network.target
-
-[Service]
-ExecStart=/usr/local/bin/dell_monitor.sh
-
-[Install]
-WantedBy=default.target" > /etc/systemd/system/dell_monitor.service
-
-
-for CONF in add_monitor dell_monitor;do
-  chmod 744 /usr/local/bin/$CONF.sh
-  chmod 664 /etc/systemd/system/$CONF.service
-  systemctl daemon-reload
-  systemctl enable $CONF.service
-done
+apt install jq -y
